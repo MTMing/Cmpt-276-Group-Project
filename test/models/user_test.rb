3ddc7b58c06cac0c74e654e973e29a77faab299b
Richard <rza47@sfu.ca>
2018-02-10 15:15:10 -0800

    Editting model to exclude bcrypt gem.
    For this iteration, we need to not use any gems, including the bcrypt gem. With that in mind, for this iteration, we won't have encrypted passwords. Thus, in order to authenticate users, we'll need to simply just check if the inputted password matches the password for that user in the db.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,41 +3,41 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   test "reject empty name" do
-    @user = User.new(name: " ", email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
+    @user = User.new(name: " ", email: "testing@test.com", password: "Testing1")
     assert_not @user.valid?
   end
 
   test "reject name that is too long" do
     x = "x" * 101
-    @user = User.new(name: x, email: "testing@test.com", password: "Testing1", password_confirmation: "Testing1")
+    @user = User.new(name: x, email: "testing@test.com", password: "Testing1")
     assert_not @user.valid?
   end
 
 
   test "reject empty email" do
-    @user = User.new(name: "bob", email: " ", password: "Testing1", password_confirmation: "Testing1")
+    @user = User.new(name: "bob", email: " ", password: "Testing1")
     assert_not @user.valid?
   end
 
   test "reject email that is too long" do
     z = ("z" * 91) + "@email.com" 
-    @user = User.new(name: "bob", email: z, password: "Testing1", password_confirmation: "Testing1")
+    @user = User.new(name: "bob", email: z, password: "Testing1")
     assert_not @user.valid?
   end
 
   test "reject blank password" do
-    @user = User.new(name: "bob", email: "testing@test.com", password: " ", password_confirmation: " ")
+    @user = User.new(name: "bob", email: "testing@test.com", password: " ")
     assert_not @user.valid?
   end
 
   test "reject password that is too short" do
-    @user = User.new(name: "bob", email: "testing@test.com", password: "Test1", password_confirmation: "Test1")
+    @user = User.new(name: "bob", email: "testing@test.com", password: "Test1")
     assert_not @user.valid?
   end
 
   test "reject password that is too long" do
     y = "T" + ("x" * 50) + "1"
-    @user = User.new(name: "bob", email: "testing@test.com", password: y, password_confirmation: y)
+    @user = User.new(name: "bob", email: "testing@test.com", password: y)
     assert_not @user.valid?
   end
 
